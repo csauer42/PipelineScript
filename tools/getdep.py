fa7aae838c2ea54a19ddfa9db384b206a98403f1
Corey Sauer <csauer42@gmail.com>
2021-09-29 15:15:11 -0600

    maybe this will break it again

--- tools/getdep.py
+++ tools/getdep.py
@@ -3,14 +3,10 @@ import yaml
 from pathlib import Path
 
 parent = Path().parent
-fname = os.path.join(parent, 'dependencies/deps.yml')
+fname = os.path.join(parent, '.azure/deps_alt.yml')
 
 with open(fname, 'r') as stream:
     deps = yaml.safe_load(stream)
 
-for dep in deps['dependencies']:
-    print('%-8s - %6s' % (dep['name'], dep['version']))
-
-testvar = ','.join([ dep['name'] for dep in deps['dependencies']])
-
-print("##vso[task.setvariable variable=testvar]%s" % (testvar))
+for dep in deps['parameters'][0]['default']:
+    print('%-10s - %s' % (dep['pkg'], dep['version']))
