d62b0f06de76a21e1ad2774acacb5d88b86cdefc
Corey Sauer <csauer42@gmail.com>
2021-09-29 11:02:36 -0600

    alt test

--- tools/getdep.py
+++ tools/getdep.py
@@ -11,4 +11,6 @@ with open(fname, 'r') as stream:
 for dep in deps['dependencies']:
     print('%-8s - %6s' % (dep['name'], dep['version']))
 
-print("##vso[task.setvariable variable=testvar]'test string'")
+testvar = 'test string'
+
+print("##vso[task.setvariable variable=testvar]%s" % (testvar))
