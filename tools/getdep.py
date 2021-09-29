1d83033f5f0859092158af09b96535db93a3721b
Corey Sauer <csauer42@gmail.com>
2021-09-29 15:52:20 -0600

    gonna break it again

--- tools/getdep.py
+++ tools/getdep.py
@@ -3,7 +3,7 @@ import yaml
 from pathlib import Path
 
 parent = Path().parent
-fname = os.path.join(parent, '.azure/deps_alt.yml')
+fname = os.path.join(parent, '.azure/dependencies.yml')
 
 with open(fname, 'r') as stream:
     deps = yaml.safe_load(stream)
