f6582ae8f8536c984997ccb287924f8b0229b7d3
Corey Sauer <csauer42@gmail.com>
2021-09-29 20:22:35 -0600

    run download, add gitignore

--- tools/getdep.py
+++ tools/getdep.py
@@ -1,6 +1,24 @@
 import os
 import yaml
+import subprocess
 from pathlib import Path
+
+def download(pkg, version, location):
+    cmd = [ 'az',
+            'artifacts',
+            'universal',
+            'download',
+            '--organization', 'https://dev.azure.com/CSCOTesting/',
+            '--project', '477fc493-2c12-4877-954a-3040372f2485',
+            '--scope', 'project',
+            '--feed', 'TestArtifacts',
+            '--name', pkg,
+            '--version', version,
+            '--path', location        
+          ]
+    subprocess.run(cmd)
+
+dldir = 'dependencies'
 
 parent = Path().parent
 fname = os.path.join(parent, '.azure/download.yml')
@@ -9,4 +27,4 @@ with open(fname, 'r') as stream:
     deps = yaml.safe_load(stream)
 
 for dep in deps['parameters'][0]['default']:
-    print('%-10s - %s' % (dep['pkg'], dep['version']))
+    download(dep['pkg'], dep['version'], dldir)
